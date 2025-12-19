c75168e164009a652654bf40b24a58e14ec320a4
alexandru-tatar <alexandru.tatar@yahoo.com>
2025-12-19 23:09:15 +0100

    hotfix: 401 bei post behoben, axios incerceptor hinzugefügt

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,17 +3,14 @@ import { keycloak } from '../auth/keycloak';
 
 const baseURL = `${import.meta.env.VITE_API_BASE_URL}/rest`;
 
-export const api = axios.create({
-  baseURL,
-});
+export const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
   const token = keycloak.token;
+
   if (token) {
-    config.headers = {
-      ...(config.headers ?? {}),
-      Authorization: `Bearer ${token}`,
-    };
+    config.headers?.set('Authorization', `Bearer ${token}`);
   }
+
   return config;
 });
